ece128fea543c027a35b2402ec154c52ccfc1b56
Padam Damai <padamdamai888@gmail.com>
2024-04-02 15:18:18 +0545

    added user detection for login

--- base/views.py
+++ base/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
+from django.contrib import messages
 from .models import Room,Topic
 from .forms import RoomForm
 from django.db.models import Q
 
+
+def login_page(request):
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(username = username)
+        except:
+            messages.error(request,"User doesn't exit")
+            
+
+    context = {}
+    return render(request,'base/login_register.html',context)
 
 def home(request):
     q =request.GET.get('q') if request.GET.get('q') != None else ''
