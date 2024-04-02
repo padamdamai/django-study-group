a20c6e206e9ac28866512144249c610974289a34
Padam Damai <padamdamai888@gmail.com>
2024-04-02 14:13:06 +0545

    added count_room

--- base/views.py
+++ base/views.py
@@ -1,21 +1,22 @@
 from django.shortcuts import render,redirect
 from .models import Room,Topic
 from .forms import RoomForm
+from django.db.models import Q
 
-
-# rooms = [
-#     {'id':1,'name':'lets learn python'},
-#     {'id':2,'name':'Design with me'},
-#     {'id':3,'name':'FrontEnd developers'},
-# ]
 
 def home(request):
     q =request.GET.get('q') if request.GET.get('q') != None else ''
-    rooms = Room.objects.filter(topic__name__icontains = q)
-    topics =Topic.objects.all()
+    rooms = Room.objects.filter(
+        Q(topic__name__icontains = q) |
+        Q(name__icontains = q) |
+        Q(description__icontains = q)
+        )
+    rooms_count = rooms.count()
+    topics = Topic.objects.all()
     context = {
         'rooms':rooms,
-        'topic':topics
+        'topic':topics,
+        'room_count':rooms_count
                }
     return render(request,'base/home.html',context)
 
