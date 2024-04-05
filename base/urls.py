88c25a4cd7dd8fb4dc0b6adc5a5de5c3bb90d5b4
Padam Damai <padamdamai888@gmail.com>
2024-04-05 16:46:48 +0545

    added profile link

--- base/urls.py
+++ base/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('register/',views.register_user,name='register'),
     path('',views.home,name="home"),
     path('room/<str:pk>/',views.room,name="room"),
+    path('userProfile/<str:pk>/',views.userProfile,name="userProfile"),
     path('create-room/',views.createRoom,name='create-room'),
     path('update-room/<str:pk>/',views.updateRoom,name='update-room'),
     path('delete-room/<str:pk>/',views.deleteRoom,name='delete-room'),
