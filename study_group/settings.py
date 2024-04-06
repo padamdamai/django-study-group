84e7a0493bde0acd338e44ad47f245d31f81120d
Padam Damai <padamdamai888@gmail.com>
2024-04-06 22:21:13 +0545

    created static folder ,inside it other two folder are made

--- study_group/settings.py
+++ study_group/settings.py
@@ -119,6 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIR = [
+    BASE_DIR / 'static' #added static folder (to know that static folder exitst to django)
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
