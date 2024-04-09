434df124ee80bcecd2ea718e593bc3e49af29c67
Padam Damai <padamdamai888@gmail.com>
2024-04-09 22:09:40 +0545

    modifield createRoom function

--- base/views.py
+++ base/views.py
@@ -114,11 +114,18 @@ def createRoom(request):
     form = RoomForm()
     topic = Topic.objects.all()
     if request.method == 'POST':
-       form = RoomForm(request.POST)
-       if form.is_valid():
-           form.save()
-           return redirect('home')
-            
+        topic_name = request.POST.get('topic')
+        topic,created = Topic.objects.get_or_create(name = topic_name)
+
+        Room.objects.create(
+           host = request.user,
+           topic = topic,
+           name= request.POST.get('name'),
+           description = request.POST.get('description'),
+
+        )
+        return redirect('home')
+    
     context = {
         'form': form,
         'topic':topic
